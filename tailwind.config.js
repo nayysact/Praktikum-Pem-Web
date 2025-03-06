3d97b878a70f989eeecefa600cdf8c5f2747ed87
Kanaya Azzahra <kanayazahra274@gmail.com>
2025-03-06 21:41:27 +0700

    Upload Latihan Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// module.exports = {
+//   content: ['./public/**/*.{html,js}'],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
